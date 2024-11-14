5621e996ca65095227a8fb0bc18d9485775507bf
Ananta Poudel <anantapoudel580@gmail.com>
2024-11-13 20:05:24 -0500

    Commited everything for A3

---
+++ TorontoWeather/TorontoWeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using TorontoWeather;
+using TorontoWeatherApp.Models;
+
+namespace TorontoWeatherApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Weather()
+    {
+        var weatherService = new WeatherService();
+        var weather = await weatherService.GetTorontoWeatherAsync();
+        ViewBag.Weather = weather;
+        return View();
+    }
+}
